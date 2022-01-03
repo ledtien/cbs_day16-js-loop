30f02e65ca2c28a6a0661cbdcc93a5202d2119b2
Tien <ldtien.1993@gmail.com>
2022-01-04 00:33:03 +0700

    fix bai5

--- assets/js/bai5.js
+++ assets/js/bai5.js
@@ -1,11 +1,23 @@
 var bai5NumberEl = document.getElementById("bai5Number");
 var bai5Txt = document.getElementById("bai5Txt");
 
+function isPrime(num) {
+  if (num < 2) return false;
+  for (var i = 2; i < num; i++) {
+    if (num % i == 0) return false;
+  }
+  return true;
+}
+
 bai5Btn.addEventListener("click", function () {
   var bai5Number = bai5NumberEl.value * 1;
   var result = "";
-  for (var i = 1; i <= bai5Number; i++) {
-    result += i + " ";
+
+  for (var i = 0; i <= bai5Number; i++) {
+    if (isPrime(i)) {
+      result += i + " ";
+    }
   }
+
   bai5Txt.innerHTML = "<div>" + result + "</div>";
 });
